4cb00c69be05fcac8584479601c9b1a7a316a991
insub4067 <>
2021-07-21 21:56:42 +0900

    update

--- models/cart.js
+++ models/cart.js
@@ -1,15 +1,35 @@
 const mongoose = require("mongoose");
 
-
 const CartSchema = new mongoose.Schema({
-
-    
-})
-
-
-
-
-
+    loginid:{
+        type: String,
+        required: true
+    },
+    username:{
+        type: String,
+        required: true
+    },
+    price :{
+        type: Number,
+        required: true
+    },
+    productname :{
+        type: String,
+        required: true
+    },
+    thumbnail :{
+        type: String,
+        required: true
+    },
+    qunatity:{
+        type: Number,
+        required: true
+    },
+    totalprice:{
+        type: Number,
+        required: true
+    },
+});
 
 CartSchema.virtual('cartId').get(function () {
     return this._id.toHexString();
--- routers/cart.js
+++ routers/cart.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const multer = require("multer");
+const Cart = require("../models/cart");
 const Product = require("../models/product");
 const authMiddleware = require("../middlewares/auth-Middleware");
 const registerValidator = require("../middlewares/register-Validater");
@@ -8,7 +9,47 @@ const fs = require("fs");
 const app = express();
 const router = express.Router();
 
+// 카트조회
+router.get("/list/:id", async(req, res) => {
+    const { loginid } = req.params;
+    const result = await Cart.find({loginid: loginid}).sort('-createdAt')
+    res.send({result})
 
+})
 
+//카트추가 authMiddleware, 
+router.post("/add", async(req, res) => {
+    const { productname,  qunatity, username,loginid } = req.body;
+    // const { username, loginid } = res.locals.user;
+    
+    const result = await Product.find({ productname: productname })
+    const thumbnail = result[0].thumbnail
+    const price = result[0].price
+    const totalprice = price*qunatity 
+    console.log(totalprice)
+    
+    const cart = new Cart({
+        username,
+        loginid,
+        username,
+        productname,
+        price,
+        thumbnail,
+        qunatity,
+        totalprice
+    })
+
+    await cart.save();
+    const response = { productname, price, thumbnail }
+    res.send({response})
+})
+
+//카트삭제 authMiddleware,
+router.delete("/delete/:id", async(req, res) => {
+    const cartId = req.params.id
+    console.log(cartId)
+    await Cart.findByIdAndDelete(cartId)
+    res.send('success')
+});
 
 module.exports = router;
